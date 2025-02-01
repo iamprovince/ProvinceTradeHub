2cd7f1de131f8c3181d849aeb8c10bc1109d8770
theGenesisio <genesisioinfo@gmail.com>
2025-02-01 18:48:59 +0100

    Tidio src updated

--- client/src/components/layout/Chatbot.jsx
+++ client/src/components/layout/Chatbot.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 const Chatbot = () => {
   useEffect(() => {
     const script = document.createElement("script");
+    // Added latest script
     script.src = "//code.tidio.co/jry7u41nhzzeiuriwcgpnrxp6xdhth2h.js";
     script.async = true;
     document.body.appendChild(script);
